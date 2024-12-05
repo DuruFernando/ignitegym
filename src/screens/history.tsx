1107b72832ef59c78a0e1c72a65ec481899a5b02
DuruFernando <fernandoduru.santos@gmail.com>
2024-12-05 07:17:55 -0300

    fetch history

--- src/screens/history.tsx
+++ src/screens/history.tsx
@@ -1,25 +1,54 @@
 import { HistoryCard } from "@components/history-card";
 import { ScreenHeader } from "@components/screen-header";
-import { VStack, Text, Heading } from "@gluestack-ui/themed";
-import { useState } from "react";
+import { ToastMessage } from "@components/toast-message";
+import { HistoryByDayDTO } from "@dtos/history-by-day-dto";
+import { VStack, Text, Heading, useToast } from "@gluestack-ui/themed";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { api } from "@services/api";
+import { AppError } from "@utils/app-error";
+import { useCallback, useEffect, useState } from "react";
 import { SectionList } from "react-native";
 
-interface ExercisesType {
- title: string,
- data: string[]
-}
 
 export function History() {
-    const [exercises, setExercises] = useState<ExercisesType[]>([
-        {
-            title: '28.11.24',
-            data: ["Puxada frontal", "Remada unilateral"]
-        },
-        {
-            title: '28.11.24',
-            data: ["Puxada frontal"]
-        },
-    ])
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
+
+    const toast = useToast()
+    const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+
+    async function fetchHistory() {
+        try {
+            setIsLoading(true)
+            const { data } = await api.get(`history`)        
+            setExercises(data)
+        } catch (error) {
+            const isAppError = error instanceof AppError
+
+            const title = isAppError ? error.message : 'Não foi possível carregar o histórico'
+
+            return toast.show({
+                placement: 'top',
+                render: ({ id }) => (
+                    <ToastMessage
+                        id={id}
+                        title="HISTÓRICO"
+                        description={title}
+                        action="error"
+                        onClose={() => toast.close(id)}
+                    />
+                )
+            })
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchHistory()
+    }, []))
 
     return (
         <VStack flex={1}>
@@ -27,8 +56,8 @@ export function History() {
 
             <SectionList
                 sections={exercises}
-                keyExtractor={item => item}
-                renderItem={() => <HistoryCard/>}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => <HistoryCard data={item}/>}
                 renderSectionHeader={({ section }) => (
                     <Heading 
                         color="$gray200" 
